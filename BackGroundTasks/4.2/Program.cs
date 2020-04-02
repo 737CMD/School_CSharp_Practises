93bb227ff1dd17dee4eafa3dbd99cc7228565496
unknown <yistarostin@gmail.com@>
2020-04-02 20:36:02 +0300

    BackGroundTask 4.2 was done

--- BackGroundTasks/4.2/Program.cs
+++ BackGroundTasks/4.2/Program.cs
@@ -26,15 +26,17 @@ namespace _4._2
         Month month;
         static int[] daysInMonthes = { 31, 29 /*Leap year*/, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
         int monthFirstDay;
-        int[,] temperature = new int[6, 7];
+        int[,] temperature;
         public MatrixWeather()
         {
+            temperature = new int[6, 7];
             FillMonth(this);
         }
         public MatrixWeather(int monthFirstDay, Month month)
         {
             this.month = month;
             this.monthFirstDay = monthFirstDay;
+            temperature = new int[6, 7];
             FillMonth(this);
         }
         public int MonthFirstDay
@@ -45,19 +47,19 @@ namespace _4._2
             }
             set
             {
-                ReFillMonth(this, value - monthFirstDay);
-                monthFirstDay = value;
+                this.temperature = ReFillMonth(this, value - monthFirstDay);
+                monthFirstDay = (value + 42) % 7;
             }
         }
         public int[,] Temperature { get => temperature; }
-        public int DaysWithTemperatureThatEqualsZero
+        public int CountDaysWithTemperatureThatEqualsTwenty
         {
             get
             {
                 int x = 0;
                 for (int i = monthFirstDay; i < daysInMonthes[(int)(month)] + monthFirstDay; ++i)
                 {
-                    x += temperature[i / 7, i % 7] == 0 ? 1 : 0;
+                    x += temperature[i / 7, i % 7] == 20 ? 1 : 0;
                 }
                 return 0;
             }
@@ -65,9 +67,9 @@ namespace _4._2
         public int MaxDelta()
         {
             int maxDelta = -19;
-            for (int i = 1 + monthFirstDay; i < DaysInMonthes.Length + monthFirstDay; ++i)
+            for (int i = 1 + monthFirstDay; i < daysInMonthes.Length + monthFirstDay; ++i)
             {
-                if (temperature[i / 7, i % 7] - temperature[(i-1) / 7, (i-1) % 7] > maxDelta)
+                if (Math.Abs(temperature[i / 7, i % 7] - temperature[(i-1) / 7, (i-1) % 7]) > maxDelta)
                 {
                     maxDelta = temperature[i / 7, i % 7] - temperature[(i - 1) / 7, (i - 1) % 7];
                 }
@@ -77,9 +79,9 @@ namespace _4._2
         public int MaxDelta(out int maxDelta)
         {
             maxDelta = -19;
-            for (int i = 1 + monthFirstDay; i < daysInMonthes[(int)(month) + monthFirstDay; ++i)
+            for (int i = 1 + monthFirstDay; i < daysInMonthes.Length + monthFirstDay; ++i)
             {
-                if (temperature[i / 7, i % 7] - temperature[(i-1) / 7, (i-1) % 7] > maxDelta)
+                if (Math.Abs(temperature[i / 7, i % 7] - temperature[(i-1) / 7, (i-1) % 7]) > maxDelta)
                 {
                     maxDelta = temperature[i / 7, i % 7] - temperature[(i - 1) / 7, (i - 1) % 7];
                 }
@@ -94,20 +96,117 @@ namespace _4._2
                 MX.temperature[i / 7, i % 7] = random.Next(16, 35 /*Subtropics*/);
             }
         }
-        static MatrixWeather ReFillMonth(MatrixWeather MX, int DayDelta)
+        public int CountDays
         {
-            MatrixWeather result = new MatrixWeather(MX.monthFirstDay + DayDelta, MX.month);
-            for (int i = MX.monthFirstDay + DayDelta; i < MatrixWeather.daysInMonthes[(int)(MX.month)] + MX.monthFirstDay + DayDelta; ++i)
+            get
             {
-                result.temperature[i / 7, i % 7] = MX.temperature[(i - DayDelta) / 7, (i - DayDelta) % 7];
+                return daysInMonthes[(int)MonthFirstDay];
+            }
+        }
+        static int[,] ReFillMonth(MatrixWeather MX, int DayDelta)
+        {
+            DayDelta += 42;
+            DayDelta %= 7;
+
+            MatrixWeather result = new MatrixWeather((MX.monthFirstDay + DayDelta) % 7, MX.month);
+            int j = result.monthFirstDay;
+            for (int i = MX.monthFirstDay; i < MatrixWeather.daysInMonthes[(int)(MX.month)] + MX.monthFirstDay; ++i)
+            {
+                result.temperature[j / 7, j % 7] = MX.temperature[i / 7, i % 7];
+                ++j;
+            }
+            return result.temperature;
+        }
+        public override string ToString()
+        {
+            string result = "Mo\tTu\tWe\tTh\tFr\tSa\tSu"; 
+            for (int i = 0; i < 42; ++i)
+            {
+                if (i % 7 == 0)
+                {
+                    result += "\n";
+                }
+                if (temperature[i / 7, i % 7] != 0) //day exists
+                {
+                    result +=  "(" +  + (i - monthFirstDay) + "," + temperature[i / 7, i % 7] + ")";
+                }
+                result += "\t";
             }
             return result;
+        }
+        public static bool operator <(MatrixWeather first, MatrixWeather second)
+        {
+            return first.month < second.month;
+        }
+        public static bool operator >(MatrixWeather first, MatrixWeather second)
+        {
+            return first.month > second.month;
+        }
+        public static MatrixWeather operator++(MatrixWeather  mv)//Prefix increment
+        {
+            mv.MonthFirstDay = mv.MonthFirstDay + 1;
+            return mv;
+        }
+        public static  MatrixWeather operator --(MatrixWeather mv)//Prefix decrement
+        {
+            mv.MonthFirstDay = mv.MonthFirstDay - 1;
+            return mv;
+        }
+        public static implicit operator bool (MatrixWeather mv)
+        {
+            foreach(int i in mv.temperature)
+            {
+                if (i < 25)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static bool operator &(MatrixWeather first, MatrixWeather second)
+        {
+            for(int i = 0; i < 42; ++i)
+            {
+                if (first[i / 7, 7 % 7] != second[i / 7, i % 7])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public int this[int i, int j]
+        {
+            get
+            {
+                return temperature[i, j];
+            }
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
+            
+            Console.WriteLine("Input month number");
+            int monthNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input month first day (0 - Mo... 6 - Su)");
+            int monthFirstDay = int.Parse(Console.ReadLine());
+            MatrixWeather mw = new MatrixWeather(monthFirstDay, (Month)(monthFirstDay));
+            Console.WriteLine(mw.ToString());
+            Console.WriteLine("Input new month first day (0 - Mo... 6 - Su)");
+            int NewmonthFirstDay = int.Parse(Console.ReadLine());
+            mw.MonthFirstDay = NewmonthFirstDay;
+            Console.WriteLine(mw.ToString());
+            Console.WriteLine("CountDaysWithTemperatureThatEqualsTwenty");
+            Console.WriteLine(mw.CountDaysWithTemperatureThatEqualsTwenty);
+            Console.WriteLine("CountDays");
+            Console.WriteLine(mw.CountDays);
+            Console.WriteLine("Input week number");
+            int weekNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input weekday number");
+            int weekdayNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Temperature on this day:" + mw[weekNumber, weekdayNumber]);
+
         }
     }
 }
