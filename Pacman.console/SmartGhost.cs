46bfaeb715806e735c055d238f7f36f0c9d2dce0
yistarostin <yistarostin@gmail.com>
2020-04-23 23:34:05 +0300

    SmartGhost movement improvements (still TODO)

--- Pacman.console/SmartGhost.cs
+++ Pacman.console/SmartGhost.cs
@@ -2,9 +2,37 @@ class SmartGhost : Character{
     public SmartGhost(Character[,] field)
         :base(field)
     {   }
-     public void Move(int x, int y, int PacmanX, int pacmanY){
+     public override void Move(int x, int y){
+         if (HasMoved){return;}
+         System.Random rnd = new System.Random();
+         Direction previousDirection = this.Direction;
+         int[] directions = {2, 1, -1, -2};
+         if (IsInField(x + (int)Direction / 2, y + (int)Direction % 2) && Field[x + (int)Direction / 2, y + (int)Direction % 2] != null && typeof(PacmanHero) == Field[x + (int)Direction / 2, y + (int)Direction % 2].GetType()){
+             throw new PacmanEaten();
+         }
+         while (!(IsInField(x + (int)Direction / 2, y + (int)Direction % 2) && (Field[x + (int)Direction / 2, y + (int)Direction % 2] == null || Field[x + (int)Direction / 2, y + (int)Direction % 2].GetType() == typeof(Berry)))){
+             Direction = (Direction)(directions[rnd.Next(4)]);
+         }
+         if (IsInField(x + (int)Direction / 2, y + (int)Direction % 2)){
+            if (Field[x + (int)Direction / 2, y + (int)Direction % 2] == null){
+                Field[x + (int)Direction / 2, y + (int)Direction % 2] = this;
+                Field[x,y] = null;
+            }
+            else if (Field[x + (int)Direction / 2, y + (int)Direction % 2].GetType() == typeof(Berry)){
+                ++PacmanHero.EatenBerries;
+                Field[x + (int)Direction / 2, y + (int)Direction % 2] = this;
+                Field[x,y] = null;
+            }
+        }
+         Field[x + (int)Direction / 2, y + (int)Direction % 2] = this;
+         Field[x, y] = null;
+         HasMoved = true;
+     }
+    bool IsInField(int x, int y){
+        return x >= 0 && x <Field.GetLength(1) && y >= 0 && y < Field.GetLength(0);
     } 
     public override void Draw(){
         System.Console.Write("S");
+        HasMoved = false;
     }
 }
